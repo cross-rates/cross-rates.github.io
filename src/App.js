f3a11976ec879741999e8e14a0123300a95d242c
Serg-Maximchuk <serg.maximchuk@gmail.com>
2020-07-21 22:55:25 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <a className="App-link" href="/">cross-rates</a>
+            </header>
+            <div className={"content-body"}>
+            </div>
+        </div>
+    );
+}
+
+export default App;
