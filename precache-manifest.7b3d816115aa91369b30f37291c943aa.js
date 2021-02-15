f1366a974ef3bcd95ec9e50259ab81d83175d478
Serg-Maximchuk <serg.maximchuk@gmail.com>
2021-02-15 06:38:11 +0000

    Deploying to master from  @ da69dbb18ca96655463256d5d3908046173ca8ae 🚀

---
+++ precache-manifest.7b3d816115aa91369b30f37291c943aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30b579efc6e99bc710399d80398f4f31",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0fe926f50c107c3092fc",
+    "url": "/static/css/main.6aa85819.chunk.css"
+  },
+  {
+    "revision": "2f8098fdf50080b580c2",
+    "url": "/static/js/2.80800d79.chunk.js"
+  },
+  {
+    "revision": "e4c6430149ae335a415e46b1d8c8695e",
+    "url": "/static/js/2.80800d79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fe926f50c107c3092fc",
+    "url": "/static/js/main.495e472d.chunk.js"
+  },
+  {
+    "revision": "6ffa0e5766691bfb59fe",
+    "url": "/static/js/runtime-main.fab323ac.js"
+  }
+]);
