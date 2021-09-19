ecc66475e2d19d130507f2f6ef410aea7e92e3dc
Serg-Maximchuk <serg.maximchuk@gmail.com>
2021-09-19 14:31:28 +0000

    Deploying to gh-pages from @ cross-rates/cross-rates.github.io@503510e2c9650864cd22fd79f10a3bb87256dad5 🚀

---
+++ precache-manifest.d4dc9982d1fa09a018bd58ba04ed15bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc1ec8dc3eb7150d991b039194960d23",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0fe926f50c107c3092fc",
+    "url": "/static/css/main.6aa85819.chunk.css"
+  },
+  {
+    "revision": "9696f9cc617e25ecf531",
+    "url": "/static/js/2.16c8b381.chunk.js"
+  },
+  {
+    "revision": "c22be92c3e36ed4c562d86e8ab93d2b5",
+    "url": "/static/js/2.16c8b381.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fe926f50c107c3092fc",
+    "url": "/static/js/main.495e472d.chunk.js"
+  },
+  {
+    "revision": "6ffa0e5766691bfb59fe",
+    "url": "/static/js/runtime-main.fab323ac.js"
+  }
+]);
